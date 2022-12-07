a30d0b885b8d2f8ed998064e57a4d810f7ed871d
RTenaille <remi.tenaille@gmail.com>
2022-12-07 10:09:06 +0100

    Update index.js

---
+++ intro-exo-js/index.js
@@ -0,0 +1,26 @@
+let rayonCercle = 50;
+console.log(rayonCercle);
+let resultat = rayonCercle + 10;
+console.log(resultat);
+resultat = rayonCercle - 5;
+
+console.log(resultat);
+resultat = rayonCercle * 2;
+
+console.log(resultat);
+
+resultat = rayonCercle / 10;
+console.log(resultat);
+
+console.log("wololooo");
+
+let message = "Ameno sancti qui cagum aqua " + rayonCercle;
+console.log(message);
+
+let age = "-20 avant JC"
+age += 1;
+console.log(age);
+
+let ageEnNombre = parseInt(age) + 1;
+ageEnNombre +=1;
+console.log(parseInt(age));
