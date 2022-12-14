98fff6292709e18a8288bc4e59040acddf587d0e
RTenaille <remi.tenaille@gmail.com>
2022-12-14 08:34:04 +0100

    Update exo-14.js

--- exo-seance-trois-js/exo-14.js
+++ exo-seance-trois-js/exo-14.js
@@ -1,3 +1,12 @@
 function reconnaitrePalindrome(pTableau){
-    
+    let lettre1 = "a";
+    let lettre2 = "a";
+    let lettre3 = "a";
+    let lettre4 = "a";
+    let lettre5 = "a";
+    let lettre6 = "a";
+    let lettre7 = "a";
+    for( i = 0; i < pTableau.length; i++){
+        
+    }
 }
