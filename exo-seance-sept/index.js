0353ecbe5f2c0ba7449fc13359520c511d46a538
RTenaille <remi.tenaille@gmail.com>
2023-01-04 11:26:29 +0100

    update tp 1

--- exo-seance-sept/index.js
+++ exo-seance-sept/index.js
@@ -1,48 +1,72 @@
-let boutonRandom = document.querySelector("#random");
-boutonRandom.addEventListener("click", changerCouleurBody);
+let boutonRandom = document.querySelector("#btnRandom");
+boutonRandom.addEventListener("click", randomiserCouleurFond);
 
-let boutonColor = document.querySelector("#color");
-boutonColor.addEventListener("click", changerCouleurMain);
+let boutonCouleur = document.querySelector("#btnColor");
+boutonCouleur.addEventListener("click", changerCouleurFond);
 
-let boutonTitle = document.querySelector("#addTitle");
-boutonTitle.addEventListener("click", modifierTitle);
+let boutonTitre = document.querySelector("#btnAddTitle");
+boutonTitre.addEventListener("click", ajouterUnTitre);
 
-let boutonPara = document.querySelector("#addPara");
-boutonPara.addEventListener("click", modifierPara);
+let boutonPara = document.querySelector("#btnAddPara");
+boutonPara.addEventListener("click", creerPara);
 
-let boutonMasquer = document.querySelector("#displayMain");
-boutonMasquer.addEventListener("click", montrerMasquerMain);
+let boutonHide = document.querySelector("#btnHide");
+boutonHide.addEventListener("click", masquerDerouler);
+
+let listePara = new Array();
 
 
-function changerCouleurBody(event){
-    document.body.style.backgroundColor= "blue";
-
-    console.log("changerCouleurBody")
+function randomiserCouleurFond(event){
+    document.body.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 }
 
-function changerCouleurMain(event){
-    console.log('changerCouleurMain');
-    console.log(event);
-    if(document.body.style.backgroundColor == "white"){
-    document.body.style.backgroundColor= "black";
-    document.body.style.color= "white";
-    } else {
-        document.body.style.backgroundColor= "white";
-        document.body.style.color= "black";
+function changerCouleurFond(event){
+    if (document.body.style.backgroundColor=="white")
+    {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+    }
+    else
+    {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
     }
 }
 
-function modifierTitle(event){
-    let newTitle = prompt("Ajoutez votre propre titre :");
-    document.querySelector("#titleMain").innerHTML= newTitle;
+function ajouterUnTitre(event){
+    let newTitle = document.createElement("h1");
+    let titre = prompt("Ajoutez votre titre :");
+    newTitle.innerText = titre;
+    let parent = document.querySelector("#mainCorpus");
+    parent.appendChild(newTitle);
 }
-function modifierPara(event){
-    let newPara = prompt("Ajoutez votre propre texte :");
-    document.querySelector("#paraMain").innerHTML= newPara;
+
+function creerPara(event){
+    let newPara = document.createElement("p");
+    let texte = prompt("Ajoutez votre texte :");
+    newPara.innerText = texte;
+    let parent = document.querySelector("#mainCorpus");
+    parent.appendChild(newPara);
+
+    let newBtnHidePara = document.createElement("button");
+    newBtnHidePara.innerText = "Cacher le paragraphe";
+    newBtnHidePara.addEventListener("click", cacherPara);
+
 
 }
 
-function montrerMasquerMain(event){
-    document.querySelector("")
+function cacherPara(event){
+    document.querySelector 
 }
+
+function masquerDerouler(event){
+    listePara.forEach( document => {
+        if (document.querySelector("p").style.display == "block"){
+            document.querySelector("p").style.display = "none";
+        } else {
+            document.querySelector("p").style.display = "block";
+        }
+    });
+}
+
 
