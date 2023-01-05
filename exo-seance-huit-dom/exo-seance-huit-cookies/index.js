235f957a53d00657df156064f74a1173f1fe7a82
RTenaille <remi.tenaille@gmail.com>
2023-01-05 14:38:38 +0100

    update cookies

---
+++ exo-seance-huit-dom/exo-seance-huit-cookies/index.js
@@ -0,0 +1,43 @@
+let boutonCouleur = document.querySelector("#btnColor");
+boutonCouleur.addEventListener("click", changerCouleurFond);
+
+let boutonClair = document.querySelector("#btnLight");
+boutonClair.addEventListener("click", remettreModeClair);
+boutonClair.style.display= "none";
+
+let darkCookie = document.cookie.set(
+    
+)
+console.log(document.cookie);
+
+let darkModeOn = false;
+let darkModeOff = true;
+
+function changerCouleurFond(event){
+    if (darkModeOn == false || darkModeOff == true)
+    {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        darkModeOn = true;
+        darkModeOff = false;
+        console.log(darkModeOff, darkModeOn);
+
+        boutonClair.style.display= "block";
+        boutonCouleur.style.display= "none";
+
+    }
+}
+
+function remettreModeClair(event){
+    if(darkModeOn == true || darkModeOff == false)
+    {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        darkModeOn = false;
+        darkModeOff = true;
+        console.log(darkModeOff, darkModeOn);
+
+        boutonClair.style.display= "none";
+        boutonCouleur.style.display= "block";
+    }
+}
